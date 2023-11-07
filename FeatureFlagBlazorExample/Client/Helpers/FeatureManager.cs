6aedeec5880b7ca1b6dd3b36449bc337274dff81
Hakim BEN AMAR <Hakim@hf-online.be>
2023-11-07 20:51:50 +0100

    First commit

---
+++ FeatureFlagBlazorExample/Client/Helpers/FeatureManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace FeatureFlagBlazorExample.Client.Helpers
+{
+
+    public interface IFeatureManager
+    {
+        bool IsEnabled(string feature);
+    }
+
+    public class FeatureManager : IFeatureManager
+    {
+        private readonly IConfiguration _configuration;
+
+        public FeatureManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool IsEnabled(string feature)
+        {
+            if (!string.IsNullOrEmpty(feature))
+            {
+                return  _configuration.GetValue<bool>($"FeatureManagement:{feature}");
+            }
+            return false;
+        }
+    }
+}
---
+++ FeatureFlagBlazorExample/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace FeatureFlagBlazorExample.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
